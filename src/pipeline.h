78e4b84659301dd41db44bb965af0b19ae2dc4a4
Guiliard <camposrafa806@gmail.com>
2024-12-28 17:23:08 -0300

    some things

--- src/pipeline.h
+++ src/pipeline.h
@@ -6,7 +6,6 @@
 
 char* instruction_fetch(cpu* cpu, ram* memory);
 type_of_instruction instruction_decode(char* instruction, unsigned short int num_instruction);
-//unsigned short int execute(cpu* cpu, type_of_instruction type, char* instruction);
 void execute(cpu* cpu, pipe *p);
 void memory_access(cpu* cpu, ram* memory_ram, type_of_instruction type, char* instruction);
 void write_back(cpu* cpu, type_of_instruction type, char* instruction, unsigned short int result);
